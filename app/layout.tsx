4c5cd0fdde10e1f17cfe431c16822cb45db8d063
Alex Hort-Francis <ajhf@hotmail.co.uk>
2025-07-12 19:07:14 +0100

    feat: Add Recursive as webfont

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Recursive } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const recursive = Recursive({
+  variable: "--font-recursive",
+  // All available variable axes for Recursive 
+  axes: ['CASL', 'CRSV', 'MONO', 'slnt'],
   subsets: ["latin"],
 });
 
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${recursive.variable} antialiased `}
       >
         {children}
       </body>
