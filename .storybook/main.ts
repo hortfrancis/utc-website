c39240f6d2299338607acf49848d983b46481632
Alex Hort-Francis <ajhf@hotmail.co.uk>
2026-03-02 11:20:43 +0000

    docs(storybook): add intro page

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,11 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: [
+    '../docs/storybook/intro.mdx',
     '../components/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
+  managerHead: (head) =>
+    head.replace(/<title>.*?<\/title>/, '<title>UTC Design System – Storybook</title>'),
   staticDirs: ['../public'],
   addons: [
     '@chromatic-com/storybook',
